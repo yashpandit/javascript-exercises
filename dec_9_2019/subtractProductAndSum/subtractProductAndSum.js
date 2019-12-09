7a71b461baeb096948d906b9307e584cc4e2eedb
Yash Pandit <yashpandit1995@gmail.com>
2019-12-09 10:43:12 +0530

    Delete subtractProductAndSum.js

--- dec_9_2019/subtractProductAndSum/subtractProductAndSum.js
+++
@@ -1,9 +0,0 @@
-const subtractProductAndSum = (n) => {
-  const digits = n.toString().split('').map(Number);
-  const multiplication = digits.reduce((mul, digit) => mul * digit, 1);
-  const sum = digits.reduce((total, digit) => total + digit, 0);
-  const difference = multiplication - sum;
-  return difference;
-};
-
-module.exports = subtractProductAndSum;
