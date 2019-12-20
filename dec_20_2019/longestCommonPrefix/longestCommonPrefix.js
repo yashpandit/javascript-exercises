f028eed941606df31d2188f0ce0ffd1eeda9971c
yashpandit <yashpandit1995@gmail.com>
2019-12-20 22:36:18 +0530

    Solve longestCommonPrefix

---
+++ dec_20_2019/longestCommonPrefix/longestCommonPrefix.js
@@ -0,0 +1,35 @@
+const indexOfShortestString = (strings) => {
+  const firstString = strings[0];
+  const shortestString = strings.reduce((acc, curr) => {
+    if (curr.length < acc.length) {
+      acc = curr;
+    }
+    return acc;
+  }, firstString);
+  return strings.indexOf(shortestString);
+};
+
+
+const getLongestCommonPrefix = (strings) => {
+  const allStrings = [...strings];
+  if (allStrings.length === 0) {
+    throw new Error('Input cannot be empty array!');
+  }
+  if (allStrings.length === 1) {
+    return allStrings[0];
+  }
+  const shortestString = allStrings.splice(indexOfShortestString(strings), 1)[0];
+  let longestCommonPrefix = '';
+  for (let i = 0; i < shortestString.length; i++) {
+    const currentCharacter = shortestString[i];
+    for (let j = 0; j < allStrings.length; j++) {
+      if (allStrings[j][i] !== currentCharacter) {
+        return longestCommonPrefix;
+      }
+    }
+    longestCommonPrefix += currentCharacter;
+  }
+  return longestCommonPrefix;
+};
+
+export { getLongestCommonPrefix };
---
+++ dec_20_2019/longestCommonPrefix/longestCommonPrefix.test.js
@@ -0,0 +1,16 @@
+import { getLongestCommonPrefix } from './longestCommonPrefix';
+
+describe('longest common prefix of given strings', () => {
+  test('should throw error if empty array passed as input', () => {
+    expect(() => { getLongestCommonPrefix([]); }).toThrow('Input cannot be empty array!');
+  });
+
+  test('should return correct output', () => {
+    expect(getLongestCommonPrefix(['wordless'])).toBe('wordless');
+    expect(getLongestCommonPrefix(['geeksforgeeks', 'geeks', 'geek', 'geezer'])).toBe('gee');
+    expect(getLongestCommonPrefix(['apple', 'ape', 'april'])).toBe('ap');
+    expect(getLongestCommonPrefix(['nap', 'nata', 'nop'])).toBe('n');
+    expect(getLongestCommonPrefix(['dap', 'nata', 'nop'])).toBe('');
+  });
+
+});
